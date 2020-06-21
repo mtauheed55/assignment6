28639319b6f72e8eaf6ba4351237b38e24c9fe2f
mtauheed55 <mtb.engineerr@gmail.com>
2020-06-21 22:19:30 +0500

    assignment6

---
+++ app1.js
@@ -0,0 +1,11 @@
+// q1.
+var firstName= prompt("enter your first name:");
+var LastName= prompt("enter your last name:");
+alert("welcome" + "  " + firstName +" " + LastName)
+// q2..
+var input = prompt("enter your favourite cell name:")
+document.write("My favourite cell is:" + "  " + input + "<br>" + "Length  of string is ="+ "  " + input.length)
+// q3..
+var str = "Pakistani";
+var i = str.indexOf("n")
+document.write("String ;" + " " + str + "<br>"   + " " + "index of 'n' :" + " " + i);
